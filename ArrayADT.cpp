73ca3c05144a8a990c27a11125c755201037a913
Çağın Kılınç <caginkilincyt@gmail.com>
2021-07-30 05:55:16 +0300

    "Array Merging" added

--- ArrayADT.cpp
+++ ArrayADT.cpp
@@ -180,12 +180,41 @@ void REarrange(struct Array* arr)
         if (i < j)swap(&arr->A[i], &arr->A[j]);
     }
 }
+struct Array* Merge(struct Array* arr1, struct Array* arr2)
+{
+    int i, j,k;
+    i = j = k = 0;
+    struct Array* arr3 = (struct Array*)malloc(sizeof(struct Array));
+    while (i < arr1->length && j < arr2->length) {
+        if (arr1->A[i] < arr2->A[j]) {
+            arr3->A[k++] = arr1->A[i++];
+        }
+        else
+        {
+            arr3->A[k++] = arr2->A[j++];
+        }
+    }
+    for (; i < arr1->length; i++)
+    {
+        arr3->A[k++] = arr1->A[i];
+    }
+    for (; j < arr2->length; j++)
+    {
+        arr3->A[k++] = arr2->A[j];
+    }
+        arr3->length = arr1->length + arr2->length;
+        arr3->size = 10;
+        return arr3;
+    }
+
 int main()
 {
-    struct Array arr = { {2,-3,25,10,-15,-7},10,7 };
-       
-    
-    Display(arr);
+    struct Array arr = { {2,6,10,15,25},10,5 };
+    struct Array arr2 = { {3,4,7,18,20},10,5 };
+    struct Array* arr3;
+    arr3 =  Merge(&arr, &arr2);
+
+    Display(*arr3);
     std::cout << "Hello World!\n";
 }
 

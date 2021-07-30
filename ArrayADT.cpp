1df04c1a0e2f7727e7038665f2f65398d03c6c30
Çağın Kılınç <caginkilincyt@gmail.com>
2021-07-31 02:15:17 +0300

    C to C++ Conversion

--- ArrayADT.cpp
+++ ArrayADT.cpp
@@ -8,12 +8,55 @@ using namespace std;
 
 int main()
 {
-    struct Array arr = { {2,6,10,15,25},10,5 };
-    struct Array arr2 = { {3,4,7,15,20},10,5 };
-    struct Array* arr3;
-    arr3 =  Difference(&arr, &arr2);
+    Array* arr1 = new Array;
+    int ch, x, index,sz;
+    printf("Enter Size of an Array");
+    scanf_s("%d", &sz);
+    arr1 =new  Array(sz);
+   
+    do {
+        printf("Menu\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Search\n");
+        printf("4. Sum\n");
+        printf("5. Display\n");
+        printf("6. Exit\n");
 
-    Display(*arr3);
+        printf("Enter your choice: ");
+        scanf_s("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            printf("Enter an element and index: ");
+            scanf_s("%d%d", &x, &index);
+            arr1->Insert(index,x);
+            break;
+        case 2:
+            printf("Enter Index: ");
+            scanf_s("%d", &index);
+            x = arr1->Delete( index);
+            printf("Deleted Element is %d\n", x);
+            break;
+        case 3:
+            printf("Enter element to search");
+            scanf_s("%d", &x);
+            index = arr1->LinearSearchM1( x);
+            printf("Element index %d", index);
+            break;
+        case 4:
+            cout << "Sum is %d\n" << arr1->Sum() << endl;
+            break;
+        case 5: arr1->Display();
+
+
+        }
+    } while (ch < 6);
+
+
+
+
     std::cout << "Hello World!\n";
 }
 
